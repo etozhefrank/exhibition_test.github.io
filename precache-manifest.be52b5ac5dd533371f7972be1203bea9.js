ddbdebb7f056f5ecafbfd5331b7dfb197dc92561
etozhefrank <51893554+etozhefrank@users.noreply.github.com>
2020-09-23 19:53:27 +0300

    Add files via upload

---
+++ precache-manifest.be52b5ac5dd533371f7972be1203bea9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fcbdccd63e4f7827d8f0126d5c0dbe4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aeaa2dde259e22f061b2",
+    "url": "/static/css/main.5470781f.chunk.css"
+  },
+  {
+    "revision": "655fe057b6d1ded4a585",
+    "url": "/static/js/2.2111f745.chunk.js"
+  },
+  {
+    "revision": "aeaa2dde259e22f061b2",
+    "url": "/static/js/main.a5221a07.chunk.js"
+  },
+  {
+    "revision": "7107b225f6f8f9831472",
+    "url": "/static/js/runtime-main.08150bc2.js"
+  },
+  {
+    "revision": "e3de665bed3d3d24e741c56ab0512716",
+    "url": "/static/media/iceland.e3de665b.mp4"
+  }
+]);
